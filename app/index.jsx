7c8886b768ab132e9c1f83514d6dbcafaf7e3552
GuFvr <gustavo.vfavero@gmail.com>
2025-03-19 23:21:39 -0300

    feat: update UI with timer, button, and styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,84 @@
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Text, View, StyleSheet, Image, Pressable } from 'react-native'
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.jsx to edit the screen.</Text>
+      <Image style={styles.logo} source={require('../assets/images/pomodoro.png')} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais..
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura.
+        </Text>
+      </View>
+      <View>
+      <StatusBar style='translucent'/>
     </View>
+    </View>
+
+    
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+    gap: 40
+  },
+  logo: {
+    flexDirection: 'column',
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: '#14448080',
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480',
+    gap: 32
+  },
+  timer: {
+    color: '#FFF',
+    fontSize: 54,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  footer: {
+    width: '80%'
+  },
+  footerText: {
+    color: "#98A0A8",
+    textAlign: "center",
+    fontSize: 12.5,
+  }
+}) 
