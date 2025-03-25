7755c3417649753ac1a1d85c29b8a8d00ad9bfaf
GuFvr <gustavo.vfavero@gmail.com>
2025-03-24 22:23:27 -0300

    refactor: extract Timer, FokusButton, and ActionButton components

---
+++ components/FokusButton/index.jsx
@@ -0,0 +1,20 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export const FokusButton = ({ onPress, title }) => (
+  <Pressable style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </Pressable>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+});
---
+++ components/Timer/index.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text } from "react-native";
+
+export const Timer = ({ totasSeconds }) => {
+  const date = new Date(totasSeconds * 1000);
+  const options = {
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  return (
+    <Text style={styles.timer}>
+      {date.toLocaleTimeString("pt-BR", options)}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
